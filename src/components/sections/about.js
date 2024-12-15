ef23f84b9226a583fce5286573f078d9050d7638
Rami L Kuttab <74058561+rlk23@users.noreply.github.com>
2024-12-15 10:50:24 -0600

    Added few stuff to it

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,16 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = [
+    'JavaScript (ES6+)',
+    'TypeScript',
+    'React',
+    'Python',
+    'Node.js',
+    'C++',
+    'AWS',
+    'Rust',
+  ];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,30 +144,24 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Rami-Lionel Kuttab, and I am a software engineer with a deep passion
+              for building innovative computer applications. I thrive on solving complex challenges
+              and turning ideas into functional, impactful solutions. My love for technology comes
+              from the endless possibilities it offers to create, optimize, and improve the way we
+              interact with the world. As a software engineer, I enjoy working on projects that push
+              the boundaries of creativity and technical problem-solving. Whether it's developing
+              robust backend systems, crafting user-friendly interfaces, or exploring emerging
+              technologies, I find joy in every step of the process.
             </p>
 
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
-            </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              <a href="https://www.bgcg.com/bgc/">a huge corporation</a>, and{' '}
+              <a href="https://www.flowai.tech/">a start-up</a>. My My main focus these days is
+              exploring the exciting fields of AI and robotics, creating innovative projects, and
+              pushing the boundaries of what's possible with technology. I’m passionate about
+              experimenting with intelligent systems and building solutions that blend creativity
+              with cutting-edge advancements.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
